c68e25bdbab64c37f2d8a337ee8a302be1f8e40f
jonathan.hickey <hickeyjonathan02@gmail.com>
2020-12-22 11:37:54 +0000

    Adding initial question from stack exchange

---
+++ src/ImprovingCodeReadability/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ImprovingCodeReadability
+{
+
+    //https://codereview.stackexchange.com/questions/253600/a-program-that-uses-three-methods-to-reverse-and-print-an-array
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string getnums = Console.ReadLine();
+            int getnum = Convert.ToInt32(getnums);
+            int[] array1 = new int[getnum];
+
+            GenerateNum(getnum, array1);
+            RevArray(array1);
+            PrintArray(array1);
+        }
+        static int[] GenerateNum(int getnums, int[] array)
+        {
+
+            for (int index = 0; index < getnums; index++)
+            {
+                array[index] = index;
+            }
+            return array;
+        }
+        static int[] RevArray(int[] array)
+        {
+            for (int index = 0; index < (array.Length) / 2; index++)
+            {
+                int a = array[index];
+                array[index] = array[array.Length - index - 1];
+                array[array.Length - index - 1] = a;
+            }
+            return array;
+        }
+        static void PrintArray(int[] array)
+        {
+            Console.WriteLine(string.Join(",", array));
+        }
+    }
+}
